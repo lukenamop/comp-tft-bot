6cc62acc6314564f1447514c5af40ef490047cec
lukenamop <lukenamop@gmail.com>
2020-09-03 15:55:59 -0400

    tft-bot v0.17.2

--- main.py
+++ main.py
@@ -630,13 +630,13 @@ def main():
 	inbox_reply_stream_process = Process(target=inbox_reply_stream, args=(mp_lock, reddit, request_headers,))
 	inbox_reply_stream_process.start()
 
-	# start the comment reply stream
-	comment_reply_stream_process = Process(target=comment_reply_stream, args=(mp_lock, reddit,))
-	comment_reply_stream_process.start()
+	# # start the comment reply stream
+	# comment_reply_stream_process = Process(target=comment_reply_stream, args=(mp_lock, reddit,))
+	# comment_reply_stream_process.start()
 
-	# start the submission reply stream
-	submission_reply_stream_process = Process(target=submission_reply_stream, args=(mp_lock, reddit,))
-	submission_reply_stream_process.start()
+	# # start the submission reply stream
+	# submission_reply_stream_process = Process(target=submission_reply_stream, args=(mp_lock, reddit,))
+	# submission_reply_stream_process.start()
 
 	# start the ranked flair updater
 	ranked_flair_updater_process = Process(target=ranked_flair_updater, args=(mp_lock, reddit, request_headers,))
