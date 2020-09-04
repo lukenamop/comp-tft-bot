ecc60d33512f6b6b0aef7d7f471ada02c8ceccf6
lukenamop <lukenamop@gmail.com>
2020-09-04 08:28:28 -0400

    tft-bot v0.17.16

--- main.py
+++ main.py
@@ -621,9 +621,10 @@ def ranked_flair_index(mp_lock, reddit):
 		# iterate through all subreddit flairs
 		rioters = []
 		for flair in subreddit.flair(limit=None):
-			if 'challenger' in flair['flair_text'].lower():
-				print(flair)
-				rioters.append(flair['user'].name)
+			if flair['flair_text'] is not None:
+				if 'challenger' in flair['flair_text'].lower():
+					print(flair)
+					rioters.append(flair['user'].name)
 		print('done iterating challenger flair!')
 		print(rioters)
 
